0a28e26fab555567b0495c0e3a57f056a692b4d7
ADTanner <sgtadt@gmail.com>
2024-08-30 13:24:42 -0500

    created service to make axios calls to backend for random activity fetching and adding favorites.

---
+++ frontend/what-should-i-do-today/src/services/activityService.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// create axios instance
+const apiClient = axios.create({
+    baseURL: 'http://localhost:8000', // match to actual backend base url
+    withCredentials: true, // sends cookies with requests
+});
+
+// fetch a random activity
+export const fetchRandomActivity = async (type?: string, participants?: number, price?: number) => {
+    try {
+        const response = await apiClient.get('/activities/fetch/', {
+            params: {type, participants, price },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching activity: ', error);
+        throw error;
+    }
+};
+
+// add a favorite activity
+export const addFavoriteActivity = async (activityData: any) => {
+    try {
+        const response = await apiClient.post('/activities/add-favorite/', activityData);
+        return response.data;
+    } catch (error) {
+        console.error('Error adding favorite activity: ', error);
+        throw error;
+    }
+};
