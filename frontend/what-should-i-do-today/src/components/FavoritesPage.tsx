052cca79c525b0074358fb056f08064c900d9174
ADTanner <sgtadt@gmail.com>
2024-08-30 10:25:30 -0500

    modified index.tsx to include BrowserRouter. set up basic routes in app.tsx. created basic components for HomePage and FavoritesPage.

---
+++ frontend/what-should-i-do-today/src/components/FavoritesPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FavoritesPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Your Faborite Activities</h1>
+            {/* This is where saved favorites will be displayed */}
+        </div>
+    );
+};
+
+export default FavoritesPage;
---
+++ frontend/what-should-i-do-today/src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>What Should I Do Today?</h1>
+            {/* This is where we will display activity suggestions */}
+        </div>
+    );
+};
+
+export default HomePage;
