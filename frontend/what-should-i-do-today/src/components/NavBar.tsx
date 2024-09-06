6a179a3e0af06b85b9623e9cf68d18f30bfc9d31
ADTanner <sgtadt@gmail.com>
2024-09-06 13:06:18 -0500

    connected home, profile, login, and signup from frontend navbar to backend.

--- frontend/what-should-i-do-today/src/components/NavBar.tsx
+++ frontend/what-should-i-do-today/src/components/NavBar.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import { Box, Flex, Link, Button, Stack } from '@chakra-ui/react';
-import { NavLink as RouterLink } from 'react-router-dom';
+import { Box, Flex, Button, Stack } from '@chakra-ui/react';
 
 const NavBar: React.FC = () => {
+
   return (
     <Box bgGradient="linear(to-t, green.500, gray.700)" px={4}>
       <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
         <Box color="white" fontWeight="bold">What Should I Do Today?</Box>
 
         <Stack direction={'row'} spacing={4}>
-          <Button as={RouterLink} to="/" variant="link" color="white">
+          <Button as="a" href="http://localhost:8000/" variant="link" color="white">
             Home
           </Button>
-          <Button as={RouterLink} to="/favorites" variant="link" color="white">
+          <Button as="a" href="http://localhost:8000/favorites" variant="link" color="white">
             Favorites
           </Button>
-          <Button as={RouterLink} to="/login" variant="link" color="white">
+          <Button as="a" href="http://localhost:8000/accounts/login" variant="link" color="white">
             Login
           </Button>
-          <Button as={RouterLink} to="/register" variant="link" color="white">
+          <Button as="a" href="http://localhost:8000/accounts/signup" variant="link" color="white">
             Register
+          </Button>
+          <Button as="a" href="http://localhost:8000/accounts/profile" variant="link" color="white">
+            Profile
           </Button>
         </Stack>
       </Flex>
